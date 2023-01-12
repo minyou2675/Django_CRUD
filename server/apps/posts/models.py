1db44ab5ca241a2867b905ee58f89f4dd9204f74
minyou2675 <kymyy2675@gmail.com>
2023-01-12 15:37:45 +0900

    CRD

--- server/apps/posts/models.py
+++ server/apps/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=32) #문자열 컬럼
+    title = models.CharField(max_length=64) #문자열 컬럼
     user = models.CharField(max_length=32)
     content = models.TextField()
     region = models.CharField(max_length=16)
---
+++ server/apps/posts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from server.apps.posts.views import posts_delete,posts_create,posts_list,posts_retrieve
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("",posts_list),
+    path("posts/posts_create",posts_create),
+    path("posts/<int:pk>",posts_retrieve),
+    path("posts/<int:pk>/delete",posts_delete),
+]
